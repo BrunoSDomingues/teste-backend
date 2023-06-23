bfa925bad36feb4c0faaf7db563da892046a85c2
BrunoSDomingues <brunao.signorelli@hotmail.com>
2023-06-23 01:39:42 -0300

    Optimized formatters

--- src/formatters.ts
+++ src/formatters.ts
@@ -1,18 +1,17 @@
-// Phone and CEP formatters
-
-// Phone: returns to format (xx) xxxx-xxxx or (xx) xxxxx-xxxx (depends on number of digits)
+// Phone: returns formatted phone number (xx) xxxx-xxxx or (xx) xxxxx-xxxx (depending on the number of digits)
 export function formatPhone(phone: string): string {
-    return phone
-        .replace(/\D/g, "")
-        .replace(/(\d{2})(\d)/, "($1)$2")
-        .replace(/(\d{4})(\d)/, " $1-$2")
-        .replace(/(\d{4})-(\d)(\d{4})/, "$1$2-$3")
-        .replace(/(-\d{4})\d+?$/, "$1");
+    const digitsOnly = phone.replace(/\D/g, "");
+    const isElevenDigits = digitsOnly.length === 11;
+    const formattedPhone = digitsOnly.replace(
+        /(\d{2})(\d{4,5})(\d{4})$/,
+        isElevenDigits ? "($1) $2-$3" : "($1) $2-$3"
+    );
+    return formattedPhone;
 }
 
+// CEP: returns formatted CEP xxxxx-xxx
 export function formatCEP(cep: string): string {
-    return cep
-        .replace(/\D/g, "")
-        .replace(/(\d{5})(\d)/, "$1-$2")
-        .replace(/(-\d{3})\d+?$/, "$1");
+    const digitsOnly = cep.replace(/\D/g, "");
+    const formattedCEP = digitsOnly.replace(/(\d{5})(\d{3})$/, "$1-$2");
+    return formattedCEP;
 }
